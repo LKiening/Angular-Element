06501848224380e45d1c6753eb89427a74611f16
Lorenz Kiening <lorenz.kiening@gmail.com>
2019-12-13 18:12:32 +0100

    added Files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Angular-Element
+# Angular Elements Example
+
+This example shows the communication patterns between an angular-element-component and another app.
+
+# Build:
+- install nodeJs
+- run the following commands:
+```
+cd angular-app
+npm install
+./custombuild.sh`
+```
+- open index.html in the root directory
+
+# Thanks to:
+- Rajat S. for his tutorial [Using Angular Elements — Why and How? — Part 1](https://blog.bitsrc.io/using-angular-elements-why-and-how-part-1-35f7fd4f0457) 
+- https://icanhazdadjoke.com/ for the api
---
+++ angular-app/src/app/component/HelloWorld/HelloWorld.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+
+@Component({
+  // selector: 'app-hello-world',
+  templateUrl: './HelloWorld.component.html',
+  styleUrls: ['./HelloWorld.component.scss']
+})
+export class HelloWorldComponent implements OnChanges {
+
+	public joke: string;
+	private header = { headers: new Headers({
+		Accept: 'application/json'
+	  })}
+		
+	@Input()  private newjoke;	// Member müssen alles kleinbuchstaben sein sonst funktioniert setAttribute in der Root-App nicht (index.html)
+	@Output() private fetched = new EventEmitter();
+
+    ngOnChanges() {
+        this.getNewJoke();
+    }
+
+    getNewJoke() {
+      	fetch('https://icanhazdadjoke.com/', this.header)
+          	.then(value => value.json())
+			.then(json => {
+				this.joke = json.joke;
+				this.fetched.emit(this.joke)
+			})
+    }
+}
